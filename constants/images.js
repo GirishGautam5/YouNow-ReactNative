4f2c6452693a863d67e07c208ff387bebcdd6971
GirishGautam5 <girishkumarnarreddy@gmail.com>
2022-04-27 01:56:35 +0530

    added images

--- constants/images.js
+++ constants/images.js
@@ -1,5 +1,22 @@
 const profile_photo = require("../assets/images/dummy_profile/profile_photo.png");
+const sample_photo = require("../assets/images/dummy_profile/1.jpg");
+
+const person1 = require("../assets/images/dummy_profile/1.jpg");
+const person2 = require("../assets/images/dummy_profile/2.jpg");
+const person3 = require("../assets/images/dummy_profile/3.jpg");
+const person4 = require("../assets/images/dummy_profile/4.jpg");
+const person5 = require("../assets/images/dummy_profile/5.jpg");
+const person6 = require("../assets/images/dummy_profile/6.jpg");
+const person7 = require("../assets/images/dummy_profile/7.jpg");
 
 export default {
     profile_photo,
+    sample_photo,
+    person1,
+    person2,
+    person3,
+    person4,
+    person5,
+    person6,
+    person7,
 }
