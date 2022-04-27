8f53720696bbe8bd8f2bfda2e05233847b216ee8
GirishGautam5 <girishkumarnarreddy@gmail.com>
2022-04-27 10:19:18 +0530

    Added extra images and videos

--- constants/icons.js
+++ constants/icons.js
@@ -8,6 +8,22 @@ const right_arrow = require("../assets/icons/right-arrow.png");
 const search = require("../assets/icons/search.png");
 const star = require("../assets/icons/star.png");
 const upload = require("../assets/icons/upload.png");
+const gold = require("../assets/icons/gold-bar.png");
+const coin = require("../assets/icons/coin-emoji.png");
+const gifts = require("../assets/icons/gifts.png");
+const video = require("../assets/icons/video.png");
+const gesture = require("../assets/icons/gesture.png");
+const like = require("../assets/icons/like.png");
+const eye = require("../assets/icons/eyes.png");
+const info = require("../assets/icons/info.png");
+const mail = require("../assets/icons/mail.png");
+const award = require("../assets/icons/award.png");
+const diamond = require("../assets/icons/diamond.png");
+const notification = require("../assets/icons/notification.png");
+const settings = require("../assets/icons/settings.png");
+const barChart = require("../assets/icons/bar-chart.png");
+const videoCircle = require("../assets/icons/videocircle.png");
+const trophy = require("../assets/icons/trophy.png");
 
 export default {
     airplay,
@@ -19,5 +35,21 @@ export default {
     right_arrow,
     search,
     star,
-    upload
+    upload,
+    gold,
+    coin,
+    gifts,
+    video,
+    gesture,
+    like,
+    eye,
+    info,
+    mail,
+    award,
+    diamond,
+    notification,
+    settings,
+    barChart,
+    videoCircle,
+    trophy,
 }
--- constants/images.js
+++ constants/images.js
@@ -1,6 +1,7 @@
 const profile_photo = require("../assets/images/dummy_profile/profile_photo.png");
 const sample_photo = require("../assets/images/dummy_profile/1.jpg");
-
+const video = require("../assets/video/sample-mp4-file-small.mp4");
+const earthVideo = require("../assets/video/Sample-Video-File-For-Testing.mp4");
 const person1 = require("../assets/images/dummy_profile/1.jpg");
 const person2 = require("../assets/images/dummy_profile/2.jpg");
 const person3 = require("../assets/images/dummy_profile/3.jpg");
@@ -12,6 +13,8 @@ const person7 = require("../assets/images/dummy_profile/7.jpg");
 export default {
     profile_photo,
     sample_photo,
+    video,
+    earthVideo,
     person1,
     person2,
     person3,
